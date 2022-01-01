0719e982c53371937d98726b2dd1a938f42bb06f
seamikaansifr <96702918+seamikaansifr@users.noreply.github.com>
2022-01-01 10:34:49 +0330

    Add files via upload

---
+++ Filtri.py
@@ -0,0 +1,195 @@
+#KAANSIFR                            
+
+import os
+# KAANSIFR
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#FILTYNG
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# code colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+#$CRIPT
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#date()
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("         Script By")
+print (Fore.YELLOW + "")
+x = """ bot running....
+  ■■■■■■■■■
+  ■                           
+  ■  █     █            
+  ■                           
+  ■ ✖✖✖✖✖               
+  ■■■■■■■■■
+  
+    .o oOOOOOOOo                                            OOOo
+             Ob.OOOOOOOo  OOOo.      oOOo.                      .adOOOOOOO
+             OboO''''''''''''.OOo. .oOOOOOo.    OOOo.oOOOOOo..''''''''OO
+             OOP.oOOOOOOOOOOO "POOOOOOOOOOOo.   "OOOOOOOOOP,OOOOOOOOOOOB'
+             O'OOOO'     OOOOo"OOOOOOOOOOO .adOOOOOOOOO"oOOO'    OOOOo
+             .OOOO'            OOOOOOOOOOOOOOOOOOOOOOOOOO'            OO
+             OOOOO                 '"OOOOOOOOOOOOOOOO"                oOO
+            oOOOOOba.                .adOOOOOOOOOOba               .adOOOOo.
+           oOOOOOOOOOOOOOba.    .adOOOOOOOOOO@^OOOOOOOba.     .adOOOOOOOOOOOO
+          OOOOOOOOOOOOOOOOO.OOOOOOOOOOOOOO"  '"OOOOOOOOOOOOO.OOOOOOOOOOOOOO
+          "OOOO"       "YOoOOOOMOIONODOO"  .   '"OOROAOPOEOOOoOY"     "OOO"
+             Y           'OOOOOOOOOOOOOO: .oOOo. :OOOOOOOOOOO?'         :
+             :            .oO%OOOOOOOOOOo.OOOOOO.oOOOOOOOOOOOO?
+                          oOOP"%OOOOOOOOoOOOOOOO?oOOOOO?OOOO"OOo
+                          '%o  OOOO"%OOOO%"%OOOOO"OOOOOO"OOO':
+                               $"  OOOO' O"Y ' `OOOO'  o
+                                      OP"          : o
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+#⁶6⁶
+print(f"{red} ")
+d = """
+ ППППППППППППППППППППППППППППППП∑∑∑∑∑∑∑∑∑∑∑∑∑∑∑████████████████
+ 
+ ███████
+ █
+ █■  ■
+ █
+ █✖✖✖
+ ███████
+   
+       //////////////////////////////////////////////////////////////////////////.......
+         .o oOOOOOOOo                                            OOOo
+             Ob.OOOOOOOo  OOOo.      oOOo.                      .adOOOOOOO
+             OboO''''''''''''.OOo. .oOOOOOo.    OOOo.oOOOOOo..''''''''OO
+             OOP.oOOOOOOOOOOO "POOOOOOOOOOOo.   "OOOOOOOOOP,OOOOOOOOOOOB'
+             O'OOOO'     OOOOo"OOOOOOOOOOO .adOOOOOOOOO"oOOO'    OOOOo
+             .OOOO'            OOOOOOOOOOOOOOOOOOOOOOOOOO'            OO
+             OOOOO                 '"OOOOOOOOOOOOOOOO"                oOO
+            oOOOOOba.                .adOOOOOOOOOOba               .adOOOOo.
+           oOOOOOOOOOOOOOba.    .adOOOOOOOOOO@^OOOOOOOba.     .adOOOOOOOOOOOO
+          OOOOOOOOOOOOOOOOO.OOOOOOOOOOOOOO"  '"OOOOOOOOOOOOO.OOOOOOOOOOOOOO
+          "OOOO"       "YOoOOOOMOIONODOO"  .   '"OOROAOPOEOOOoOY"     "OOO"
+             Y           'OOOOOOOOOOOOOO: .oOOo. :OOOOOOOOOOO?'         :
+             :            .oO%OOOOOOOOOOo.OOOOOO.oOOOOOOOOOOOO?
+                          oOOP"%OOOOOOOOoOOOOOOO?oOOOOO?OOOO"OOo
+                          '%o  OOOO"%OOOO%"%OOOOO"OOOOOO"OOO':
+                               $"  OOOO' O"Y ' `OOOO'  o
+                                      OP"          : o
+"""
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+# Mjtba *_*
+#my cat
+#   ️❤️❤️❤️❤️❤️❤️
+#   💙💙💙💙💙💙      
+#   💚💚💚💚💚💚  
+#   💜💜💜💜💜💜
+
+time.sleep(4.3)
+print(f"{green} ")
+print ("ok welcome friends . ❤️ . ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                       ID RUBIKA KAANSIFR")
+print ("                         ID telegram KAANSIFR")
+print ("                          New Id rubikaKAANSIFR ")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("__________")
+time.sleep(0.7)
+print ("✖_________")
+time.sleep(0.6)
+print ("✖✖________")
+time.sleep(0.5)
+print ("✖✖✖_______")
+time.sleep(0.4)
+print ("✖✖✖✖______")
+time.sleep(0.3)
+print ("✖✖✖✖✖_____")
+time.sleep(0.2)
+print ("✖✖✖✖✖✖____")
+time.sleep(0.1)
+print ("✖✖✖✖✖✖✖___")
+time.sleep(0.5)
+print ("✖✖✖✖✖✖✖✖__")
+time.sleep(0.1)
+print ("✖✖✖✖✖✖✖✖✖_")
+time.sleep(0.5)
+print ("〇〇〇〇〇〇〇〇〇")
+print ("█████████████████████████")
+print ("")
+# vared kardane addad baraye gereftan kod
+up = input(" 1 > > > ")
+print(f"{pink} ")
+time.sleep(3.0)
+print("""
+  update =
+$        ❤️
+$       💙
+$       💚
+""")
+
+print(f"{red} ")
+
+
+
+if up == "1":
+
+        print ('code for Channel ≠>' + '%s%s(3.5.6.7.4.6.3.5.6.3.5.6.3.5.6.3.6.6.3.6.8.5.3.5.6.3.6.//h//f.4.6.3.5.6.3.5.6)%s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+if up != "1":
+        print("error - lotfan dorost vared konid")
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""
+Kanal haye rubikam =
+KAANSIFR KAANSIFR
+ """)
+print("""
+link gap rubikam ==>>>
+rubika.almaytm.almaytm.almaytm
+""")
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print (" Bye Bye My Friend :)🙂❤️")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print ('Baraye Inke Yeki Ro Filter Konid In Ro Befrestin' + '%s%s  /  %s' % (fg('red'), bg('white'), attr('reset')))
+print (f"{blue} ")
+time.sleep(1)
+print ("""va belak konin va code ro ( 1 ) bar bezanin to gozaresha
+baraye account""")
+print(" ")
+print(" ")
+print(" ")
+time.sleep(2.0)
+os.system("cd $HOME")
+os.system("rm -rf /data/data/com.termux/files/home/Filtering-Rubika && git clone https://github.com/C4ssif3r/Filtering-Rubika")
+print ("time end :")
+print (Fore.WHITE + "")
+date()
